9760a98724d4bacbf973104ffff84d175537f23c
NikolaS <nikolaevs1006@gmail.com>
2026-04-27 10:16:50 +0300

    refactor(user): delete unusable type orm entity import

--- server/src/users/users.module.ts
+++ server/src/users/users.module.ts
@@ -1,12 +1,8 @@
 import { Module } from '@nestjs/common';
 import { UsersService } from './users.service';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { UserEntity } from './user.entity';
 
 @Module({
   providers: [UsersService],
-  imports: [TypeOrmModule.forFeature([UserEntity])], // forFeature([Entity]) делает только одну вещь: он создает Repository для этой сущности ...
-  // ... и делает его доступным для внедрения (Dependency Injection) в этом модуле.
   exports: [UsersService],
 })
 export class UsersModule {}
