e6d7f526f965dabcc678036758073f8e8b838c3d
NikolaS <nikolaevs1006@gmail.com>
2026-04-28 08:12:40 +0300

    fix(server): delete unusable type orm config from app module

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -9,43 +9,24 @@ import Joi from 'joi';
 
 @Module({
   imports: [
-    ConfigModule.forRoot({ // config module это динамический модуль который надо настроить, 
-    // ... root говорит о том, что конфигурация, которую мы передаём ...
-    // ...будет существовать в единственном экземпляре (Singleton) на уровне всего приложения.
+    ConfigModule.forRoot({
       isGlobal: true,
       validationSchema: Joi.object({
         JWT_ACCESS_SECRET: Joi.string().required(),
         JWT_REFRESH_SECRET: Joi.string().required(),
-        JWT_ACCESS_EXPIRES_IN: Joi.number().required(),
-        JWT_REFRESH_EXPIRES_IN: Joi.number().required(),
+        JWT_ACCESS_EXPIRES: Joi.number().required(),
+        JWT_REFRESH_EXPIRES: Joi.number().required(),
         POSTGRES_PORT: Joi.number().port().required(),
         POSTGRES_HOST: Joi.string().required(),
-        POSTGRES_DB_NAME: Joi.string().required(),
+        POSTGRES_DB: Joi.string().required(),
         POSTGRES_USER: Joi.string().required(),
         POSTGRES_PASSWORD: Joi.string().required(),
-      }),
-    }),
-    TypeOrmModule.forRootAsync({ // изспользует forRootAsync потому что надо дождаться загрузки ConfigModule 
-      inject: [ConfigService], // inject позволяет внедрить зависимость которую мы будет ожидать ...
-      // Плюсы ConfigService над простым взятием через process.env:
-      // 1. Безопасность: ConfigService гарантирует, что данные уже ЗАГРУЖЕНЫ и провалидированы(Joi).
-      // 2. Типизация: Можно явно указать тип .get<number>(...), избегая ручного приведения строк.
-      // 3. Тесты: Позволяет легко подменять настройки в тестах, не трогая системные переменные.
-      useFactory: (configService: ConfigService) => ({ // с помощью use factory и ConfigService мы коллбеком передаём значения из configModule в TypeOrmModule
-        type: 'postgres',
-        port: configService.get<number>('POSTGRES_PORT'),
-        host: configService.get<string>('POSTGRES_HOST'),
-        database: configService.get<string>('POSTGRES_DB'),
-        username: configService.get<string>('POSTGRES_USER'),
-        password: configService.get<string>('POSTGRES_PASSWORD'),
-        autoLoadEntities: true, // автоматически собирает все сущности зареганые через forFeature
-        synchronize: true, 
       }),
     }),
     UsersModule,
     AuthModule,
     NotesModule,
-    PrismaModule
-  ] 
+    PrismaModule,
+  ],
 })
 export class AppModule {}
