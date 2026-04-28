73ee21a3073b5c4df8b0a82d757f3052d84ecf45
NikolaS <nikolaevs1006@gmail.com>
2026-04-28 08:11:40 +0300

    fix(prisma): activate cjs imports to fix imports error

--- server/prisma/generated/internal/class.ts
+++ server/prisma/generated/internal/class.ts
@@ -20,7 +20,7 @@ const config: runtime.GetPrismaClientConfig = {
   "clientVersion": "7.8.0",
   "engineVersion": "3c6e192761c0362d496ed980de936e2f3cebcd3a",
   "activeProvider": "postgresql",
-  "inlineSchema": "// This is your Prisma schema file,\n// learn more about it in the docs: https://pris.ly/d/prisma-schema\n\n// Get a free hosted Postgres database in seconds: `npx create-db`\n\ngenerator client {\n  provider = \"prisma-client\"\n  output   = \"../prisma/generated\"\n}\n\ndatasource db {\n  provider = \"postgresql\"\n}\n\nmodel User {\n  id String @id @default(uuid())\n\n  email          String  @unique\n  username       String  @unique\n  hashedPassword String? @map(\"hashed_password\")\n\n  createdAt DateTime @default(now()) @map(\"created_at\")\n  notes     Note[]\n\n  @@map(\"users\")\n}\n\nmodel Note {\n  id String @id @default(uuid())\n\n  title          String?\n  text           String?\n  colorKey       colorKey @default(FIRST) @map(\"color_key\")\n  status         status   @default(DEFAULT)\n  positionNumber Int      @map(\"position_number\")\n\n  createdAt DateTime @default(now()) @map(\"created_at\")\n  updatedAt DateTime @updatedAt @map(\"updated_at\")\n\n  authorId String @map(\"author_id\")\n  author   User   @relation(fields: [authorId], references: [id], onDelete: Cascade)\n\n  @@map(\"tasks\")\n}\n\nenum colorKey {\n  FIRST\n  SECOND\n  THIRD\n  FOURTH\n  FIFTH\n  SIXTH\n  SEVENTH\n  EIGHTH\n  NINTH\n  TENTH\n  ELEVENTH\n  TWELVEFTH\n}\n\nenum status {\n  DEFAULT\n  ARCHIVED\n  TRASHED\n}\n",
+  "inlineSchema": "// This is your Prisma schema file,\n// learn more about it in the docs: https://pris.ly/d/prisma-schema\n\n// Get a free hosted Postgres database in seconds: `npx create-db`\n\ngenerator client {\n  provider     = \"prisma-client\"\n  output       = \"../prisma/generated\"\n  moduleFormat = \"cjs\"\n}\n\ndatasource db {\n  provider = \"postgresql\"\n}\n\nmodel User {\n  id String @id @default(uuid())\n\n  email          String  @unique\n  username       String  @unique\n  hashedPassword String? @map(\"hashed_password\")\n\n  createdAt DateTime @default(now()) @map(\"created_at\")\n  notes     Note[]\n\n  @@map(\"users\")\n}\n\nmodel Note {\n  id String @id @default(uuid())\n\n  title          String?\n  text           String?\n  colorKey       colorKey @default(FIRST) @map(\"color_key\")\n  status         status   @default(DEFAULT)\n  positionNumber Int      @map(\"position_number\")\n\n  createdAt DateTime @default(now()) @map(\"created_at\")\n  updatedAt DateTime @updatedAt @map(\"updated_at\")\n\n  authorId String @map(\"author_id\")\n  author   User   @relation(fields: [authorId], references: [id], onDelete: Cascade)\n\n  @@map(\"tasks\")\n}\n\nenum colorKey {\n  FIRST\n  SECOND\n  THIRD\n  FOURTH\n  FIFTH\n  SIXTH\n  SEVENTH\n  EIGHTH\n  NINTH\n  TENTH\n  ELEVENTH\n  TWELVEFTH\n}\n\nenum status {\n  DEFAULT\n  ARCHIVED\n  TRASHED\n}\n",
   "runtimeDataModel": {
     "models": {},
     "enums": {},
@@ -45,10 +45,10 @@ async function decodeBase64AsWasm(wasmBase64: string): Promise<WebAssembly.Modul
 }
 
 config.compilerWasm = {
-  getRuntime: async () => await import("@prisma/client/runtime/query_compiler_fast_bg.postgresql.mjs"),
+  getRuntime: async () => await import("@prisma/client/runtime/query_compiler_fast_bg.postgresql.js"),
 
   getQueryCompilerWasmModule: async () => {
-    const { wasm } = await import("@prisma/client/runtime/query_compiler_fast_bg.postgresql.wasm-base64.mjs")
+    const { wasm } = await import("@prisma/client/runtime/query_compiler_fast_bg.postgresql.wasm-base64.js")
     return await decodeBase64AsWasm(wasm)
   },
 
