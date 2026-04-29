573b13f5aa70f1ee5d5e55a4fa86adc4d7381dd9
NikolaS <nikolaevs1006@gmail.com>
2026-04-29 11:04:25 +0300

    chore(root): delete ClassSerializerInterceptor in main.ts

--- server/src/main.ts
+++ server/src/main.ts
@@ -5,19 +5,16 @@ import cookieParser from 'cookie-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  
+
   app.use(cookieParser());
 
-  app.useGlobalPipes(new ValidationPipe({
-    whitelist: true,              // Отрезает всё, что не описано в DTO
-    forbidNonWhitelisted: true,   // Вместо отрезания выбрасывает ошибку 400
-    transform: true,              // Превращает типы (строки в числа и т.д.)
-  }));
-
-  app.useGlobalInterceptors(new ClassSerializerInterceptor(
-    app.get(Reflector), 
-    { strategy: 'excludeAll' }
-  ))
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true, // Отрезает всё, что не описано в DTO
+      forbidNonWhitelisted: true, // Вместо отрезания выбрасывает ошибку 400
+      transform: true, // Превращает типы (строки в числа и т.д.)
+    }),
+  );
 
   await app.listen(process.env.PORT ?? 3000);
 }
